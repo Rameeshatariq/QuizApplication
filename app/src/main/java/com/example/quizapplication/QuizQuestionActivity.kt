52a0ec4f72ee222e6a516d9b418ebd699aa86e20
Rameeshatariq <35740845+Rameeshatariq@users.noreply.github.com>
2021-10-04 22:14:53 +0500

    [Questions Added]

--- app/src/main/java/com/example/quizapplication/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapplication/QuizQuestionActivity.kt
@@ -2,10 +2,17 @@ package com.example.quizapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.log10
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        val questionsList = Constants.getQuestions()
+
+        Log.i("000888", "onCreate: ${questionsList.size}")
+
     }
 }
