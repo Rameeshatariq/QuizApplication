7bd38772d2449b5cffdd1a34158399f425c09eb5
Rameeshatariq <35740845+Rameeshatariq@users.noreply.github.com>
2021-10-04 22:52:15 +0500

    [Questions Added]

--- app/src/main/java/com/example/quizapplication/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapplication/QuizQuestionActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 import kotlin.math.log10
 
 class QuizQuestionActivity : AppCompatActivity() {
@@ -13,6 +14,18 @@ class QuizQuestionActivity : AppCompatActivity() {
         val questionsList = Constants.getQuestions()
 
         Log.i("000888", "onCreate: ${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition-1]
+
+        progressBar.progress = (currentPosition)
+        tvProgress.text = "$currentPosition"+"/"+progressBar.max
+        tvQuestion.text = question!!.question
+        ivFlag.setImageResource(question.correctAnswer)
+        tvOption1.text = questionsList[0].optionOne
+        tvOption2.text = questionsList[0].optionTwo
+        tvOption3.text = questionsList[0].optionThree
+        tvOption4.text = questionsList[0].optionFour
 
     }
 }
